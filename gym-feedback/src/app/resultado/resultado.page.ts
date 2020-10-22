745599440fdfadd1738ad89f2258440ba3a45f27
LariSmith <lena.dracarys@gmail.com>
2020-10-22 20:37:17 -0300

    criando slides para resultado

--- gym-feedback/src/app/resultado/resultado.page.ts
+++ gym-feedback/src/app/resultado/resultado.page.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultadoPage implements OnInit {
 
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
+
   constructor() { }
 
   ngOnInit() {
